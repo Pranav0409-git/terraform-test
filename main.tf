006ca67533a90a1297e7fa34bc7349b8937a3125
EC2 Default User <ec2-user@ip-172-31-22-59.ec2.internal>
2026-01-29 14:53:53 +0000

    Add EC2 resource via Terraform

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ resource "aws_s3_bucket" "test" {
 resource "random_id" "id" {
   byte_length = 4
 }
+# ---------- EC2 INSTANCE ----------
+resource "aws_instance" "test_ec2" {
+  ami           = "ami-0532be01f26a3de55"   # Amazon Linux 2 (us-east-1)
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "jenkins-tf-ec2"
+  }
+}
